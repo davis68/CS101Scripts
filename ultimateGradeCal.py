e61fcffa8469337c71114a0955e6de781990fd38
Neal Davis <davis68@illinois.edu>
2018-09-20 17:11:56 -0500

    Post fix.

--- ultimateGradeCal.py
+++ ultimateGradeCal.py
@@ -1,67 +1,90 @@
-# coding= latin-1
-import glob, sys
-import string
-from nbgrader.api import Gradebook
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from getCollabs import getCollabs
+def readConfiguration():
+    '''
+    Load the config.py file stored adjacent to this script.
+    '''
+    import yaml
+    configData = yaml.safe_load( open( './config.yml' ) )
+    try:
+        pass
+    except:
+        raise Exception( 'You don\'t seem to have a readable configuration file in this directory.' )
+    return configData
 
-'''
-getGradeFromFile(filename) opens a file given by extract_grades.py
-in the format where each line is
-netid,score
-and return the grades as a dictionary.
-'''
-def getGradeFromFile(filename):
-    grades = {} #Use a dictionary
-    with open(filename) as f:
-        for line in f.readlines():
-            line = line.split()
-            grades[line[0]] = float(line[1])
-        collabs = {}
-        if len(sys.argv)==5: # Using collabs
-            collabs = getCollabs(sys.argv[3], sys.argv[4])
-        for submitter in collabs:
-            netidlist = collabs [submitter]
-            if submitter in grades: # They should be, if not, print error I guess
-                for netid in netidlist:
-                    if netid in grades:
-                        grades[netid] = grades[submitter]
+
+def extractGrades( gradebookDB,section,labNo ):
+    '''
+    Open the specified grade database and return grades.
+
+    Input:
+        filename    str
+
+    Output:
+        grades      dict
+    '''
+    import sys
+
+    from nbgrader.api import Gradebook
+    gb = Gradebook( gradebookDB )
+    #gb = Gradebook( f'sqlite:////class/cs101/etc/sxns/{section}/gradebook.db')
+
+    grades = {}
+    for s in gb.assignment_submissions( "lab" + labNo ):
+        grades[ s.student_id ] = float( s.score )
     return grades
 
-'''
-getCollabs(filename) opens a file given by collabs.py
-in the format where each line is
-netid [netid netid ...]
-and return a dictionary where the submitter is the key
-'''
-def getCollabsFromFile(filename):
+
+def getCollabs(labSec, labNo):
+    '''
+    Identify collaborating pairs of students.
+
+    Collaborators are listed near each other at the top of the notebook in question.
+    '''
+    import glob
+    import json
+    import path
+    import sys
+
     collabs = {}
-    with open(filename) as f:
-        for line in f.readlines():
-            if (line==''):
-                continue
-            line2 = ""
-            line = line.strip()
-            for c in line:
-                if c in string.printable:
-                    line2 += c
-            line = line2.split(' ')
-            submitter = line[0]
-            collabsList = line[1:]
-            for i in range(len(collabsList)):
-                collabsList[i] = collabsList[i].strip()
-            collabs[submitter] = collabsList
+    files = glob.glob( f'/class/cs101/etc/sxns/{labSec}/submitted/*/lab{labNo}/*.ipynb' )
+
+    from string import whitespace as w
+    from string import punctuation as p
+    from string import printable
+
+    collabsLabel = '''List _all_ collaborators' or partners' **NetIDs** below, including your own:'''
+
+    for file in files:
+        # Load IPYNB in JSON format.
+        data = json.load( open( file ) )
+
+        # Locate collaborator label.  The collaborators are listed in the next cell.
+        for cell in data[ 'cells' ][ 0:12 ]:
+            if collabsLabel in cell['source'][ 0 ]:
+                break
+        collabCell = data[ 'cells' ][ data[ 'cells' ].index( cell ) + 1 ]
+
+        # Parse out collaborators, which may be raw names or strings.
+        if len( collabCell[ 'source' ] ) < 1:
+            # No collaborators have been listed.
+            break
+
+        names = collabCell[ 'source' ]
+        for c in w + p:
+            names = names.replace( c,' ' )
+        nameList = names.split()
+        nameList = [ name for name in names if name != '' ]
+        nameList = [ name.strip( ',' ) for name in names ]
+        nameList = [ name.strip( '\'' ) for name in names ]
+        nameList = [ name.strip( '"' ) for name in names ]
+
+        collabsList = ''.join( names ).split()
+        submitter = os.path.split( file )[ -3 ]
+        collabs[ submitter ] = collabsList
     return collabs
 
-def parseGrades(grades):
-    maxGrade = grades[max(grades, key=grades.get)]
-    if maxGrade != 0:
-        for key, value in grades.items():
-            grades[key] = value / maxGrade * 2.0
-    else:
-        for key, value in grades.items():
-            grades[key] = 2.0
-    return grades
 
 def parseCollabs(content, collabs,colNo):
     if len(collabs)==0:
@@ -78,72 +101,90 @@ def parseCollabs(content, collabs,colNo):
                     else:
                         content[word][colNo] = content[collab][colNo] = "2.0" #unable it so that everyone gets 2.0
                 # print(content[word][colNo],content[collab][colNo])
-import argparse
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract grades from NB grader and output compass compatible csv files.')
-    parser.add_argument('csv', metavar='gc...csv', nargs='?', help='csv downloaded from Compass (comma separated)')
-    parser.add_argument('section', metavar='{A-Q}', nargs='?', help='section names (multiple supported)')
-    parser.add_argument('no' , metavar='XX', nargs='?', help='labNo')
-    parser.add_argument('-c', '--collabs', metavar="Y/N", nargs = '?', help = "Fetch collaborator information", default="Y")
-    parser.add_argument('-o', '--output', metavar = 'output', nargs = '?', help = 'csv file to upload to compass', default = sys.stdout)
 
-    args = parser.parse_args()
-    csvfilename = args.csv
-    labSec = args.section
-    aNo = args.no if len(args.no) == 2 else '0' + args.no
-    sys.stdout = open(args.output, 'w') if args.output != sys.stdout else sys.stdout
-    from extract_grades import extract_grades
+
+def parseArguments():
+    '''
+    Parse arguments.
+    '''
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser( description='''Extract grades from NB grader and output compass compatible csv files.\n\nUsage:\n\tpython3 ultimateGradeCal.py 3\nto read lab03.''' )
+    parser.add_argument( 'lab',nargs=1,help='lab' )
+    parser.add_argument( '-c','--collabs',metavar="Y/N",nargs = '?',help="Fetch collaborator information",default="Y" )
+    parser.add_argument( '-o','--output',metavar='output',nargs='?',help='csv file to upload to compass',default = sys.stdout )
+
+    return parser.parse_args()
+
+
+def parseGrades( grades,scale ):
+    '''
+    Parse grades into proper scaling for grading (specified by scale).
+    '''
+    maxGrade = grades[ max( grades,key=grades.get ) ]
+    if maxGrade == 0:
+        # When the maximum grade is zero, it is a MATLAB lab or otherwise participation--based.
+        for key, value in grades.items():
+            grades[ key ] = 2.0
+    else:
+        # Otherwise, scale appropriately from the highest grade.
+        for key,value in grades.items():
+            grades[ key ] = value / maxGrade * scale
+    return grades
+
+
+def writeGradesOut( grades,labNo,collabs,outputFileName ):
+    '''
+    Write grades out to a Compass-compatible CSV file.  This can be a single
+    column or the entire gradebook.  By default, Compass outputs UTF-16-encoding.
+    '''
+    import pandas as pd
+
+    df = pd.read_csv( outputFileName,encoding='utf-16',delimiter='\t' )
+    df = df.set_index( 'Username',drop=False )
+
+    for column in df.columns:
+        if 'lab '+labNo in column.lower() or 'lab'+labNo in column.lower():
+            break
+
+    for student in grades:
+        #print( student,grades[ student ] )
+        df.loc[ student ][ column ] = grades[ student ]
+        if student in collabs:
+            for collab in collabs[ student ]:
+                df.loc[ collab ][ column ] = grades[ student ]
+
+    df.to_csv( outputFileName )
+
+def main():
+    # Read in configuration. ###################################################
+    args = parseArguments()
+    config = readConfiguration()
+
+    gradebookDB    = config[ 'gradebookDB' ]
+    csvFileName    = config[ 'rosterFile' ]
+    labSec         = config[ 'section' ]
+    labNo          = args.lab[ 0 ] if len( args.lab[ 0 ] ) == 2 else '0' + args.lab[ 0 ]
+    gradeScale     = config[ 'scale' ]
+    outputFileName = config[ 'outputFile' ]
+    if config[ 'debug' ]: print( csvFileName,labSec,labNo,gradeScale,outputFileName )
+
+    # Read in grades. ##########################################################
     grades = {}
-    for labSecChar in labSec:
-        grades.update(extract_grades(labSecChar,aNo))
-    # from getValue0 import getHw1Score
-    # grades = getHw1Score()
-    # print (grades)
-    
-    # from lab01grades import lab01grades
-    # grades = lab01grades
-    grades = parseGrades(grades)
+    grades.update( extractGrades( gradebookDB,labSec,labNo ) )
+    grades = parseGrades( grades,gradeScale )
+    if config[ 'debug' ]: print( grades )
+
+    # Identify collaborators. ##################################################
     collabs = {}
     if args.collabs == 'Y':
-        for labSecChar in labSec:
-            collabs.update(getCollabs(labSecChar,aNo))
-        # print (collabs)
-    with open(csvfilename,'r') as f:
-        fileContent = f.readlines()
-        headers = fileContent[0].strip().split(',')
-        colNo = -1
-        netidNo = -1
-        sectionNo = -1
-        availNo = -1
-        fileOutput = {} # dict of lists
-        for i in range(len(headers)):
-            if headers[i].find("lab"+str(aNo))>-1:
-                colNo = i
-            if headers[i].find("Username")>-1:
-                netidNo = i
-            if headers[i].find("Section")>-1:
-                sectionNo = i
-            if headers[i].find("Availability")>-1:
-                availNo = i
-        if colNo == -1 or netidNo == -1 or sectionNo == -1:
-            raise ValueError("CSV file doesn't have all necessary columns.")          
-        for line in fileContent[1:]:
-            line = line.strip().split(',')
-            netid = line[netidNo].strip('"')
-            # print (netid)
-            if netid in grades:
-                line[colNo] = str(grades[netid])
-                # print (line[colNo])
-                fileOutput[netid]=(line.copy())
-            else:
-                line[colNo] = '0' # Set empty ones to 0
-                fileOutput[netid]=(line.copy())
-        parseCollabs(fileOutput, collabs,colNo)
-        print (headers[netidNo]+','+headers[colNo])
-        for netid in fileOutput:
-            section = fileOutput[netid][sectionNo].strip()[-2]
-            if section in labSec: # and fileOutput[netid][availNo].find('Yes')>-1:
-                line = ','.join([fileOutput[netid][netidNo], fileOutput[netid][colNo]])
-                # line = ','.join(fileOutput[netid])
-                print (line)
+        collabs.update( getCollabs( labSec,labNo ) )
+    if config[ 'debug' ]: print( collabs )
 
+    # Format grades. ###########################################################
+    writeGradesOut( grades,labNo,collabs,outputFileName )
+
+
+if __name__ == "__main__":
+    main()
