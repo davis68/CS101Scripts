6b8e038b72cc4b6c46051dd61033c4ee5cb65d48
Neal Davis <davis68@illinois.edu>
2019-02-15 13:33:38 -0600

    Fix getCollabs.py.

--- getCollabs.py
+++ getCollabs.py
@@ -1,16 +1,19 @@
 import sys, glob
-def getCollabs(labSec, labNo):
-    collabs = {} # to return
-    files = glob.glob("/class/cs101/etc/sxns/AY"+labSec+"/submitted/*/lab"+labNo+"/netids.txt")
+def getCollabs( labSec,labNo ):
+    collabs = {}
+    labNo = str( labNo ) if int( labNo ) >= 10 else '0' + str( labNo )
+    files = glob.glob( f"/class/cs101/etc/sxns/AY{labSec}/submitted/*/lab{labNo}/netids.txt" )
     from string import whitespace as w
     from string import punctuation as p
     from string import printable
     for f in files:
-        with open(f) as openF:
-            submitter = path[path.index('submitted')+1]
+        with open( f ) as openF:
+            path_elems = f.split( '/' )
+            submitter = path_elems[ path_elems.index( 'submitted' )+1 ]
             data = openF.read().strip()
-            collabs[submitter] = data.split()
-    print (collabs)
+            #print( f,'\n',submitter,':',data )
+            collabs[ submitter ] = data.split()
+    #print ( collabs )
     return collabs
 
 if __name__ == '__main__':
@@ -20,4 +23,9 @@ if __name__ == '__main__':
     parser.add_argument('no' , metavar='XX', nargs='?', help='labNo')
     
     args = parser.parse_args()
-    print (getCollabs(args.section, args.no))
+    collabs = getCollabs(args.section, args.no)
+    for collab in collabs:
+        print( f'{collab}:  ',end='' )
+        for collaborator in collabs[ collab ]:
+            print( f'{collaborator},',end='' )
+        print()
